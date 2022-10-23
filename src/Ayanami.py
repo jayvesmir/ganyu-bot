1c5a2cb8958b7b62cc523a7b35176d86531376ff
AMEXif <amex.if.email@seznam.cz>
2022-10-23 02:41:14 +0200

    First Slash Commands Implementation

--- src/Ayanami.py
+++ src/Ayanami.py
@@ -4,11 +4,38 @@ TOKEN = json.loads(open('config.json', 'r').read())['token']
 
 import discord
 from discord.ext import commands
+from discord import app_commands
 bot: commands.Bot = commands.Bot(command_prefix="g.", intents=discord.Intents.all())
 
 from logger import log
 
 import random
+
+class Ganyu(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
+        
+    async def on_ready(self):
+        await tree.synced(guild=discord.Object(id=980162247299059792))
+        self.synced = True
+        log.info(f"Ganyu is online as {bot.user}.")
+        
+    async def on_member_join(member):
+        channel = member.guild.system_channel
+        await channel.send(f'{member.mention} Welcome To The Server!')
+        
+bot = Ganyu()
+tree = app_commands.CommandTree(bot)
+
+@tree.command(name='ping', description='Check if the bot lives.', guild=discord.Object(id=980162247299059792))
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Pong! ({Ganyu.latency*1000:.2f}ms)")
+    
+@tree.command(name='eightball', description='Rolls the 8ball.', guild=discord.Object(id=980162247299059792))
+async def self(interaction: discord.Interaction, query: str):
+    responses = ["It is certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
+    await interaction.response.send_message(f'**Question:** {query}\n**Answer:** {random.choice(responses)}')
 
 # Events
 @bot.event
