979c2aa8b9f5ede76e7f7d785902b8d946b1f7ce
AMEXif <amex.if.email@seznam.cz>
2022-10-23 02:42:37 +0200

    First Slash Command Implementation

--- src/Ayanami.py
+++ src/Ayanami.py
@@ -30,46 +30,48 @@ tree = app_commands.CommandTree(bot)
 
 @tree.command(name='ping', description='Check if the bot lives.', guild=discord.Object(id=980162247299059792))
 async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(f"Pong! ({Ganyu.latency*1000:.2f}ms)")
+    await interaction.response.send_message(f"Pong! ({bot.latency*1000:.2f}ms)")
     
 @tree.command(name='eightball', description='Rolls the 8ball.', guild=discord.Object(id=980162247299059792))
 async def self(interaction: discord.Interaction, query: str):
     responses = ["It is certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
     await interaction.response.send_message(f'**Question:** {query}\n**Answer:** {random.choice(responses)}')
 
-# Events
-@bot.event
-async def on_ready():
-    log.info(f"Ganyu is online as {bot.user}.")
+## Legacy
 
-@bot.event
-async def on_member_join(member):
-    channel = member.guild.system_channel
-    await channel.send(f'{member.mention} Welcome To The Server!')
+# # Events
+# @bot.event
+# async def on_ready():
+#     log.info(f"Ganyu is online as {bot.user}.")
+
+# @bot.event
+# async def on_member_join(member):
+#     channel = member.guild.system_channel
+#     await channel.send(f'{member.mention} Welcome To The Server!')
     
-# Commands
-@bot.command()
-async def ping(ctx):
-    await ctx.reply(f"Pong! ({bot.latency*1000:.2f}ms)")
+# # Commands
+# @bot.command()
+# async def ping(ctx):
+#     await ctx.reply(f"Pong! ({bot.latency*1000:.2f}ms)")
     
-@bot.command(aliases=['8ball'])
-async def eightball(ctx, *, query):
-    responses = ["It is certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
-    await ctx.send(f'**Question:** {query}\n**Answer:** {random.choice(responses)}')
+# @bot.command(aliases=['8ball'])
+# async def eightball(ctx, *, query):
+#     responses = ["It is certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.","As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.","Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
+#     await ctx.send(f'**Question:** {query}\n**Answer:** {random.choice(responses)}')
     
-@bot.command(aliases=['avatar'])
-async def av(ctx, member: discord.Member = None):
-    if member == None:
-        member = ctx.author
+# @bot.command(aliases=['avatar'])
+# async def av(ctx, member: discord.Member = None):
+#     if member == None:
+#         member = ctx.author
         
-    name = member.name + '#' + member.discriminator
-    pfp = member.display_avatar
-    color = member.color
+#     name = member.name + '#' + member.discriminator
+#     pfp = member.display_avatar
+#     color = member.color
     
-    embed = discord.Embed(title='Avatar', color=color)
-    embed.set_author(name=name, icon_url=pfp.url)
-    embed.set_image(url=pfp.url)
+#     embed = discord.Embed(title='Avatar', color=color)
+#     embed.set_author(name=name, icon_url=pfp.url)
+#     embed.set_image(url=pfp.url)
     
-    await ctx.send(embed=embed)
+#     await ctx.send(embed=embed)
 
 bot.run(TOKEN)
